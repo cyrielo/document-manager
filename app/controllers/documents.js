5307d1489a58c869e0eb9bab67c435c4f133cd4c
Ologho Cyril Paul <cyril.paul@andela.com>
2016-11-24 14:49:27 +0100

    Refactored document.js for better quality

--- app/controllers/documents.js
+++ app/controllers/documents.js
@@ -1,143 +1,168 @@
-class Document{
+class Document {
 
-  constructor(){
+  constructor() {
     this.jwt = require('jsonwebtoken');
     this.config = require('./../config/config');
     this.models = require('./../models/index');
+    const Validator = require('./../helpers/validate');
+
+    this.validate = new Validator();
   }
 
-  createDoc(req, res){
-
+  /*
+  * Creates a new document entry in the database
+  * @method createDoc
+  * @param {Object} req
+  * @param {Object} res Object
+  * @return undefined
+  * */
+  createDoc(req, res) {
     const verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
-    this.models.documents.createDoc(req, verifyToken.id, verifyToken.role).then((document)=>{
-      res.status(201).json({
-        status: 'success',
-        message: 'Document created successfully',
-        data: document
-      });
-    }).catch((error)=>{
-      res.status(403).json({
-        status: 'fail',
-        message: error
+    this.models.documents.createDoc(req, verifyToken.id, verifyToken.role)
+      .then((document) => {
+        res.status(201).json({
+          status: 'success',
+          message: 'Document created successfully',
+          data: document,
+        });
       })
-    });
+      .catch((error) => {
+        res.status(403).json({
+          status: 'fail',
+          message: error,
+        });
+      });
   }
 
-  all(req, res){
-    const
-      limit = req.query.limit,
-      offset = req.query.offset,
-      byRole = req.query.role,
-      byDate = req.query.date,
-      verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
+  /**
+  * Retrieves documents from the database
+  * @param {Object} req
+  * @param {Object} res
+  * */
+  all(req, res) {
+    const limit = req.query.limit;
+    const offset = req.query.offset;
+    const byRole = req.query.role;
+    const byDate = req.query.date;
+    const verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
+
     this.models.documents.all(limit, offset, byRole, byDate, verifyToken.id, verifyToken.role)
-      .then((docs)=>{
+      .then((docs) => {
         res.status(200).json({
           status: 'success',
           message: 'Documents listed',
-          data: docs
-        })
-      }).catch((errorDetails)=>{
-      res.status(errorDetails.statusCode).json({
-        status: 'fail',
-        message: errorDetails.message
+          data: docs,
+        });
+      })
+      .catch((errorDetails) => {
+        res.status(errorDetails.statusCode).json({
+          status: 'fail',
+          message: errorDetails.message,
+        });
       });
-    })
   }
-
-  getDoc(req, res){
+  /**
+  * Retrieves a specific document from the database
+  * @param {Object} req
+  * @param {Object} res
+  * */
+  getDoc(req, res) {
     const docId = req.params.id;
     const verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
     this.models.documents.getDoc(verifyToken.id, docId)
-      .then((document)=>{
+      .then((document) => {
         res.status(200).json({
           status: 'success',
           message: 'Document info loaded',
-          data: document
-        })
-      }).catch((errorDetails)=>{
+          data: document,
+        });
+      })
+      .catch((errorDetails) => {
         res.status(errorDetails.statusCode).json({
           status: 'fail',
-          message: errorDetails.message
+          message: errorDetails.message,
         });
-    })
+      });
   }
-
-  updateDoc(req, res){
+  /**
+   * Update the database with new info
+   * @param {Object} req
+   * @param {Object} res
+   * */
+  updateDoc(req, res) {
     const verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
     const docId = req.params.id;
     const title = req.body.title;
     const content = req.body.content;
     const access = req.body.access;
 
-    const Validator = require('./../helpers/validate');
-    const validate = new Validator();
+    const update = {};
+    const errors = [];
 
-    let update = {}, errors = [];
-
-    if(!validate.isEmpty(title)){
+    if (!this.validate.isEmpty(title)) {
       update.title = title;
-    }else if(validate.isDefined(title)){
+    } else if (this.validate.isDefined(title)) {
       errors.push('Title is empty');
     }
 
-    if(!validate.isEmpty(content)){
+    if (!this.validate.isEmpty(content)) {
       update.content = content;
-    }else if(validate.isDefined(content)){
+    } else if (this.validate.isDefined(content)) {
       errors.push('Content is empty');
     }
 
-    if(!validate.isEmpty(access)){
+    if (!this.validate.isEmpty(access)) {
       update.access = access;
-    }else if(validate.isDefined(access)){
+    } else if (!this.validate.isDefined(access)) {
       errors.push('Access is empty');
     }
 
-    if(errors.length < 1){
+    if (errors.length < 1) {
       this.models.documents.update(docId, verifyToken.id, update)
-        .then((document)=>{
+        .then((document) => {
           res.status(201).json({
             status: 'success',
             message: 'Document created successfully',
-            data: document
+            data: document,
           });
-        }).catch((errorDetails)=>{
-        res.status(errorDetails.statusCode).json({
-          status: 'fail',
-          message: errorDetails.message
         })
-      });
-    }else{
+        .catch((errorDetails) => {
+          res.status(errorDetails.statusCode).json({
+            status: 'fail',
+            message: errorDetails.message,
+          });
+        });
+    } else {
       res.status(403).json({
         status: 'fail',
         message: 'You have errors in data submitted',
-        data: errors
+        data: errors,
       });
     }
-
-
   }
 
-  deleteDoc(req, res){
-
+  /**
+   * Deletes a specific document from the database
+   * @param {Object} req
+   * @param {Object} res
+   * */
+  deleteDoc(req, res) {
     const verifyToken = this.jwt.verify(req.headers.authorization, this.config.secrete);
     const docId = req.params.id;
 
-   // console.log(verifyToken);
     this.models.documents.deleteDoc(docId, verifyToken.id)
-      .then(()=>{
-      res.status(200).json({
-        status: 'success',
-        message: 'Document deleted successfully',
-      });
-    }).catch((errorDetails)=>{
-      res.status(errorDetails.statusCode).json({
-        status: 'fail',
-        message: errorDetails.message
+      .then(() => {
+        res.status(200).json({
+          status: 'success',
+          message: 'Document deleted successfully',
+        });
       })
-    });
+      .catch((errorDetails) => {
+        res.status(errorDetails.statusCode).json({
+          status: 'fail',
+          message: errorDetails.message,
+        });
+      });
   }
-
 }
-
 module.exports = Document;
